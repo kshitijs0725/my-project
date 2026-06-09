f4b5e5f423f53f50b0d494b37bf900dc8bb540de
Kshitij Shambharkar <kshitij_shambharkar@intuit.com>
2026-06-10 03:36:28 +0530

    fix: fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
+          <h1>My App</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
@@ -29,7 +29,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Count is {count}!
         </button>
       </section>
 
